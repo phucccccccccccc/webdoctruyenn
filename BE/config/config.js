d3ce4edc4e807b45c884657c73c07c9c407248f6
hoangphuc <phucdanghaihoang@gmail.com>
2026-06-04 13:32:08 +0700

    commit

--- BE/config/config.js
+++ BE/config/config.js
@@ -2,9 +2,18 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 export const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error("DB Error:", err);
+        return;
+    }
+    console.log("MySQL Connected");
 });
